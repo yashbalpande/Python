45bbb9538d0719db0bbeb1bcabe3904475b588d5
Yash Prakash Balpande <145757143+yashbalpande@users.noreply.github.com>
2024-02-01 02:39:44 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,38 @@
+print("Hello_World") 
+name = input('Enter Your Name :- ')
+age = input('Enter Your Age :- ')
+College_Name = input('Enter Your College  :- ')
+print("Thank You For Your Information" "\n",name, age, College_Name)
+
+a = input('Enter Value of a :- ')
+b = input('Enter Value of b :- ')
+
+a = int(a)
+b = int(b)
+add = a + b
+"\n"
+sub = a - b 
+"\n"
+mul = a*b 
+"\n"
+div = a/b 
+"\n"
+print(add, sub, mul, div)
+
+a = int(input("Enter The area of Suqre:- "))
+
+area = a*a
+
+print("Area Of Square Is:- ", area)
+25
+
+y = float(input("Enter the Number :- "))
+z = float(input("Enter the Number :- "))
+
+
+d = (y+z)/2
+
+print("Avrage of 2 Number is :- ",d)
+
+
+
