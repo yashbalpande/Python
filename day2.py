cd712068a5fbc06e2afbca149611d74ca612745f
Yash Prakash Balpande <145757143+yashbalpande@users.noreply.github.com>
2024-02-01 02:42:23 +0530

    day2.py

--- day2.py
+++ day2.py
@@ -1,47 +1,44 @@
 
-# # # # # str = "yash_balpande"
-# # # # # print(len(str))
-# # # # str = "yash balpande"
-# # # # # ch = str[4:]
-# # # # # print(ch)
-# # # # # print(str.upper())
-# # # # print(str.replace("yash", "Gau"))
-# # # # print(str.find("a")) 
+ str = "yash_balpande"
+print(len(str))
+ str = "yash balpande"
+ ch = str[4:]
+print(ch)
+ print(str.upper())
+print(str.replace("yash", "Gau"))
+ print(str.find("a")) 
 
 
 
-# # # #Q,1
+ #Q,1
 
-# # name = input("Enter Your Name :- ")
-
-# # # print(len(name))
-
+name = input("Enter Your Name :- ")
+ print(len(name))
 
 
-# # # Q.2
 
-# # print(name.find("p"))
+# Q.2
+
+ print(name.find("p"))
 
 # # ------------------------------------------------------------------------------
 
-# x = int(input("Your age :-"))
+ x = int(input("Your age :-"))
 
-# if x >= 18:
-#     print("Your are eligible for voting card")
-# else:
-#     print("You r not eligible for votting card wait till 18th birthday")
+ if x >= 18:
+     print("Your are eligible for voting card")
+ else:
+     print("You r not eligible for votting card wait till 18th birthday")
 
 
 
-# x = int(input("Enter The Number :- "))
+ x = int(input("Enter The Number :- "))
+ rev = x % 2
 
- 
-# rev = x % 2
-
-# if (rev == 0):
-#     print("x is even number")
-# else:
-#     print("x is odd number")
+if (rev == 0):
+     print("x is even number")
+ else:
+     print("x is odd number")
 
 
 a = int(input('Enter The value of a :- '))
